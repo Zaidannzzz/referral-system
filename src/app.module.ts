16e5e312b34d0ba800ad04a8024e4e691da54df1
Zaidannzzz <zaidan.zulhakim@gmail.com>
2024-07-25 22:57:25 +0700

    fix sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,28 @@ import { PurchasesModule } from './purchases/purchases.module';
 import { User } from './users/users.entity';
 import { Purchase } from './purchases/purchases.entity';
 
+import { join } from 'path';
+import { existsSync, mkdirSync, copyFileSync } from 'fs';
+
+const tmpDir = '/tmp';
+const dbFile = join(__dirname, 'src/db/database.sqlite');
+const tmpDbFile = join(tmpDir, 'src/db/database.sqlite');
+
+if (!existsSync(tmpDir)) {
+  mkdirSync(tmpDir);
+}
+
+if (!existsSync(tmpDbFile)) {
+  copyFileSync(dbFile, tmpDbFile);
+}
+
+process.env.DB_FILE = tmpDbFile;
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: process.env.DB_FILE || 'src/db/database.sqlite',
+      database: process.env.DB_FILE,
       entities: [User, Purchase],
       synchronize: true,
     }),
